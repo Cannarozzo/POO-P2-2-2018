db3f0f3659798360f91bc215349086eb58c97516
Cannarozzo <felipe.cannarozzo@gmail.com>
2018-12-07 03:48:34 +0000

    Primeiro commit

---
+++ src/Menu.java
@@ -0,0 +1,15 @@
+
+public class Menu implements View{
+    
+    public void principal(){
+        
+    }
+    
+    public void erro(){
+        
+    }
+    
+    public void mostrarResultado(){
+        
+    }
+}
---
+++ src/View.java
@@ -0,0 +1,6 @@
+
+public interface View {
+    void principal();
+    void erro();
+    void mostrarResultado();
+}
